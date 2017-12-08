d037c3c8488d3bf2408aa6c8f2dcd66d2283cba4
Johannes Engler <johannes5117@gmail.com>
2017-12-08 15:37:01 +0100

    Test

--- train_agent.py
+++ train_agent.py
@@ -37,40 +37,28 @@ historyLength = 4
 # # both train_data and valid_data contain tupes of images and labels
 train_data = trans.get_train()
 valid_data = trans.get_valid()
-print(valid_data[0].shape)
-print(valid_data[1].shape)
+print(train_data[0].shape)
+print(train_data[1].shape)
 
-xdata = np.array(valid_data[0])
+xdata = np.array(train_data[0])
 
 
-newShape = np.reshape(xdata, (500,historyLength,25,25))
+newShape = np.reshape(xdata, (xdata.shape[0],100,25))
 print(newShape.shape)
-newShapeSafe = newShape
-print(newShapeSafe.shape)
-newShape2 = newShape[1,:,:,:]
-newShape = newShape[0,:,:,:]
+newShape = np.rot90(newShape, axes=(1,2))
+newShape = np.reshape(xdata, (xdata.shape[0],25,25,4))
 
-newShape[newShape > 50] = 2
-newShape[newShape > 17] = 1
+print(newShape.shape)
 
+#newShape = newShape[:,0::5,0::5,:]
 
-newShape2[newShape2 > 50] = 2
-newShape2[newShape2 > 17] = 1
+newShape[newShape>50] = 2
+newShape[newShape>10] = 1
 
-newShape = np.asarray(newShape, dtype=np.uint8)
-newShape2 = np.asarray(newShape2, dtype=np.uint8)
-
+print(newShape[0].shape)
+newShape = newShape[0][0::5,0::5,:][:,:,0]
 print(newShape)
 
-
-newShape = newShape[:, 0::5, 0::5]
-newShape2 = newShape2[:, 0::5, 0::5]
-
-newShape = newShape.reshape(-1,newShape.shape[1])
-np.savetxt("tetAll0.txt", newShape.astype(int),fmt='%i')
-
-newShape2 = newShape2.reshape(-1,newShape2.shape[1])
-np.savetxt("tetAll1.txt", newShape2.astype(int),fmt='%i')
 
 
 # 
@@ -83,41 +71,27 @@ np.savetxt("tetAll1.txt", newShape2.astype(int),fmt='%i')
 ######################################
 
 batch_size = 128
-num_classes = 10
+num_classes = 5
 epochs = 12
 
-# input image dimensions
-#img_rows, img_cols = 5, 5 * historyLength
-img_rows, img_cols = 28, 28
 
-# the data, shuffled and split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
-
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
+x_train = newShape[0:490,:,:,:]
+x_test =  newShape[490:500,:,:,:]
 print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
 # convert class vectors to binary class matrices
+y_train = train_data[1][0:490]
+y_test =  train_data[1][490:500]
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),
                  activation='relu',
-                 input_shape=input_shape))
+                 input_shape=(4,25,25)))
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
