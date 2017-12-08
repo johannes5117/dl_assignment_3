d29fd7d4f96d7c801d5ef5f228544a088d46ebab
Johannes Engler <johannes5117@gmail.com>
2017-12-08 14:18:48 +0100

    commit init

---
+++ train_agent.py
@@ -0,0 +1,131 @@
+import numpy as np
+import tensorflow as tf
+
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+
+# custom modules
+from utils     import Options
+from simulator import Simulator
+from transitionTable import TransitionTable
+
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# NOTE:
+# this script assumes you did generate your data with the get_data.py script
+# you are of course allowed to change it and generate data here but if you
+# want this to work out of the box first run get_data.py
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# 0. initialization
+opt = Options()
+sim = Simulator(opt.map_ind, opt.cub_siz, opt.pob_siz, opt.act_num)
+trans = TransitionTable(opt.state_siz, opt.act_num, opt.hist_len,
+                             opt.minibatch_size, opt.valid_size,
+                             opt.states_fil, opt.labels_fil)
+
+# 1. train
+######################################
+# TODO implement your training here!
+# you can get the full data from the transition table like this:
+#
+# # both train_data and valid_data contain tupes of images and labels
+train_data = trans.get_train()
+valid_data = trans.get_valid()
+print(valid_data[0].shape)
+print(valid_data[1].shape)
+
+xdata = np.array(valid_data[0])
+
+
+newShape = np.reshape(xdata, (500,4,25,25))
+print(newShape.shape)
+newShape = newShape[0,:,:,:]
+newShape[newShape > 50] = 2
+newShape[newShape > 17] = 1
+
+
+newShape = np.asarray(newShape, dtype=np.uint8)
+
+print(newShape)
+
+
+newShape = newShape[:, 0::5, 0::5]
+np.savetxt("tetNorm.txt", newShape[2,:,:].astype(int),fmt='%i')
+
+newShape = newShape.reshape(-1,newShape.shape[1])
+
+np.savetxt("tetAll.txt", newShape.astype(int),fmt='%i')
+# 
+# alternatively you can get one random mini batch line this
+#
+# for i in range(number_of_batches):
+#     x, y = trans.sample_minibatch()
+# Hint: to ease loading your model later create a model.py file
+# where you define your network configuration
+######################################
+exit()
+batch_size = 128
+num_classes = 10
+epochs = 12
+
+# input image dimensions
+img_rows, img_cols = 28, 28
+
+# the data, shuffled and split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+
+# 2. save your trained model
+
+
